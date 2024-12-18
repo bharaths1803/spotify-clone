22f6313e73ee3ac9f1c0578cbc1eabf56bc8606c
bharaths1803 <bharaths14051803@gmail.com>
2024-12-18 16:22:37 +0530

    Changing reuqire to import again and again

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,4 @@
-import animatePlugin from "@tailwindcss/animate";
+import animatePlugin from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
