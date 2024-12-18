98300ab41052909fae9c20815c1f2875f4fd35d4
bharaths1803 <bharaths14051803@gmail.com>
2024-12-18 15:44:27 +0530

    Changed typo in index.js backend file

--- backend/src/index.js
+++ backend/src/index.js
@@ -68,7 +68,7 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(app.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
